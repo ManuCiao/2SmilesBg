092e872b3f996451f924f0c143d0d005e16e8507
ManuCiao <mn.sabatino@gmail.com>
2017-11-01 16:13:52 +0000

    Add pagination to the posts - listing only 3 posts on the main page

--- SmilesBlog/blog/views.py
+++ SmilesBlog/blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 def post_list(request):
     """Create a list of posts"""
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliverr te first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page oof results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  { 'page': page,
+                    'posts': posts})
 
 def post_detail(request, year, month, day, post):
     """Create a view that displays the single post"""
