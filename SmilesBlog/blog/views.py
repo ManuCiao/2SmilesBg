d7d8cd24335be06463a5e4caefecb0f2dba4bb8a
ManuCiao <mn.sabatino@gmail.com>
2017-11-06 17:27:16 +0000

    Sending emails with Django 0- setting up the SMTP server to the views.py

--- SmilesBlog/blog/views.py
+++ SmilesBlog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
 from .forms import EmailPostForm
 
@@ -42,6 +43,7 @@ def post_detail(request, year, month, day, post):
 def  post_share(request, post_id):
     # Retrieve posts by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
 
     if request.method == 'POST':
         # form was submitted if we get a POST request,
@@ -54,7 +56,13 @@ def  post_share(request, post_id):
             # if the form data does not validaatee the cleaned_data will
             # have only the valid fields. it is a dictionary of form fields
             # and their values.
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends your reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'mn.sabatino@gmail.com', [cd['to']])
+            sent = True
         else:
             form = EmailPostForm()
         return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form})
+                                                        'form': form,
+                                                        'sent': sent})
