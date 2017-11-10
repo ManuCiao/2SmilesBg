19477eabaa3c92310654e7c388a0d9799b7d8069
ManuCiao <mn.sabatino@gmail.com>
2017-11-10 17:05:12 +0000

    creating forms from models

--- SmilesBlog/blog/forms.py
+++ SmilesBlog/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -6,3 +7,10 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+    """use of ModelForm to build a form dynamically from the Comment model """
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+        
