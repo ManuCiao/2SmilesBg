fa181d553b278c419c7ef9394230f340c14eaf70
ManuCiao <mn.sabatino@gmail.com>
2017-11-10 15:59:32 +0000

    Add the new Comment model to the Admin site to control comments using Django interface

--- SmilesBlog/blog/admin.py
+++ SmilesBlog/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'publish','status')
@@ -11,3 +11,11 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
 
 admin.site.register(Post, PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    """Add the comment model to the Admin site to manage commets via interface"""
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+admin.site.register(Comment, CommentAdmin)
