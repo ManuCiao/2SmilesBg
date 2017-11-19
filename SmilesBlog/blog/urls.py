87e8b9d38cea23fd1d0f7b9b6d253a4a84da64bc
ManuCiao <mn.sabatino@gmail.com>
2017-11-19 20:31:03 +0000

    Filter posts by tags

--- SmilesBlog/blog/urls.py
+++ SmilesBlog/blog/urls.py
@@ -2,16 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # add post views using regex
-    # url(r'^$', views.post_list, name='post_list'),
-    url(r'^$',
-        views.PostListView.as_view(),
-        name='post_list'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
-        r'(?P<post>[-\w]+)/$',
+    # post views
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+    #url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
-    url(r'^(?P<post_id>\d+)/share/$',
-        views.post_share,
-        name='post_share'),
+    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
 ]
