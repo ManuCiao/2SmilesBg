1144854a06918ae240f42002837ca516764f2886
ManuCiao <mn.sabatino@gmail.com>
2017-11-19 20:56:14 +0000

    Retrieve posts by similarity

--- SmilesBlog/blog/views.py
+++ SmilesBlog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count # Count aggregation function of the Django ORM
 
 from taggit.models import Tag
 
@@ -62,10 +63,17 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', 
+                                                                              '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html', {'post': post,
                                             'comments': comments,
-                                            'comment_form': comment_form})
+                                            'comment_form': comment_form,
+                                            'similar_posts': similar_posts})
 
 def  post_share(request, post_id):
     # Retrieve posts by id
