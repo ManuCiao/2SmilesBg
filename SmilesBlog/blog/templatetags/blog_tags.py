2f642635f5f273ff6d7e54fc8ecc8d3b0db9e634
ManuCiao <mn.sabatino@gmail.com>
2017-11-22 13:13:01 +0000

    Include latest posts on the side bar using inclusion_tag

--- SmilesBlog/blog/templatetags/blog_tags.py
+++ SmilesBlog/blog/templatetags/blog_tags.py
@@ -7,3 +7,9 @@ from ..models import Post
 @register.simple_tag  #Processes the data and returns a string - add (e.g. name='my_tag')
 def total_posts():
     return Post.published.count()
+
+# an inclusion_tag ca render a template with context variable returned by your template tag
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish') [:count]
+    return {'latest_posts': latest_posts}
