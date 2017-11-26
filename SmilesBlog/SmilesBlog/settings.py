775af5e54ea568644cd152146589931eae77aff1
ManuCiao <mn.sabatino@gmail.com>
2017-11-26 02:36:59 +0000

    Add PostgresSQL database to the blog

--- SmilesBlog/SmilesBlog/settings.py
+++ SmilesBlog/SmilesBlog/settings.py
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'SmilesBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smilesblog',
+        'USER': 'manuciao',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
