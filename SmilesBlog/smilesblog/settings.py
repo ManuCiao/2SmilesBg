fc2c3d9f6b9494a575ee9c7b9cc57659162c4d2f
ManuCiao <mn.sabatino@gmail.com>
2017-12-05 22:03:15 +0000

    Add more variables

--- SmilesBlog/smilesblog/settings.py
+++ SmilesBlog/smilesblog/settings.py
@@ -40,6 +40,8 @@ AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_QUERYSTRING_AUTH = False
+AWS_UPLOAD_USER = "smilesbloguser"
+AWS_UPLOAD_GROUP = "manage-smilesblogmedia"
 #tell Django-storages the domain to use to refer to static files
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
