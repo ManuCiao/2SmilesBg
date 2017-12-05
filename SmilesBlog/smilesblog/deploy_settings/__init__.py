6d3b20dfb041e050846462bf6261338a79d2dced
ManuCiao <mn.sabatino@gmail.com>
2017-12-05 22:51:39 +0000

    damn heroku is not working

--- SmilesBlog/smilesblog/deploy_settings/__init__.py
+++ SmilesBlog/smilesblog/deploy_settings/__init__.py
@@ -17,9 +17,14 @@ AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = get_env_variable("AWS_S3_REGION_NAME")
 AWS_UPLOAD_USER = get_env_variable("AWS_UPLOAD_USER")
 AWS_UPLOAD_GROUP = get_env_variable("AWS_UPLOAD_GROUP")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_QUERYSTRING_AUTH = False
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 #STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
-STATICFILES_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
