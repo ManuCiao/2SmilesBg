ba061c172bf4897ab4062714fe1c2499c4da2f7a
ManuCiao <mn.sabatino@gmail.com>
2017-12-10 15:34:00 +0000

    Adding tests to models2

--- SmilesBlog/blog/tests.py
+++ SmilesBlog/blog/tests.py
@@ -18,4 +18,22 @@ class PostModelTests(TestCase):
         self.assertLess(post.publish, now)
         self.assertLess(post.updated, now)
 
-        
+class CommentModelTests(TestCase):
+    def test_comment_creation(self):
+        user = User(0)
+        post = Post.objects.create(
+            title = "Test Post number 1",
+            slug = "test-post-number-1",
+            author = user,
+            body = "Description of the Post test..."
+        )
+        comment = Comment.objects.create(
+            post = post,
+            name = "CiaBella",
+            email = "ciabella@gmail.com",
+            body = "Great post..",
+            active = True
+        )
+        now = timezone.now()
+        self.assertLess(comment.created, now)
+        self.assertLess(comment.updated, now)
