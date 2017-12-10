2d3d32897feb2e154b1b0d1afb4e11d3bd99277c
ManuCiao <mn.sabatino@gmail.com>
2017-12-10 14:25:33 +0000

    Adding tests

--- SmilesBlog/blog/tests.py
+++ SmilesBlog/blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post, Comment
+
+class PostModelTests(TestCase):
+    def test_post_creation(self):
+        user = User(0)
+        post = Post.objects.create(
+            title = "Test Post number 1",
+            slug = "test-post-number-1",
+            author = user,
+            body = "Description of the Post test..."
+        )
+        now = timezone.now()
+        self.assertLess(post.created, now)
+        self.assertLess(post.publish, now)
+        self.assertLess(post.updated, now)
+
+        
