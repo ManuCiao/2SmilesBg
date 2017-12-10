a609cb34ab579b7df2fe735319bcd63dbdb74e38
ManuCiao <mn.sabatino@gmail.com>
2017-12-10 16:46:07 +0000

    keep testing the models

--- SmilesBlog/blog/tests.py
+++ SmilesBlog/blog/tests.py
@@ -4,36 +4,29 @@ from django.contrib.auth.models import User
 
 from .models import Post, Comment
 
-class PostModelTests(TestCase):
-    def test_post_creation(self):
-        user = User(0)
-        post = Post.objects.create(
+class BlogModelTests(TestCase):
+    def setUp(self):
+        self.user = User(0)
+        self.post = Post.objects.create(
             title = "Test Post number 1",
             slug = "test-post-number-1",
-            author = user,
+            author = self.user,
             body = "Description of the Post test..."
         )
-        now = timezone.now()
-        self.assertLess(post.created, now)
-        self.assertLess(post.publish, now)
-        self.assertLess(post.updated, now)
-
-class CommentModelTests(TestCase):
-    def test_comment_creation(self):
-        user = User(0)
-        post = Post.objects.create(
-            title = "Test Post number 1",
-            slug = "test-post-number-1",
-            author = user,
-            body = "Description of the Post test..."
-        )
-        comment = Comment.objects.create(
-            post = post,
+        self.comment = Comment.objects.create(
+            post = self.post,
             name = "CiaBella",
             email = "ciabella@gmail.com",
             body = "Great post..",
             active = True
         )
-        now = timezone.now()
-        self.assertLess(comment.created, now)
-        self.assertLess(comment.updated, now)
+        self.now = timezone.now()
+
+    def test_post_creation(self):
+        self.assertLess(self.post.created, self.now)
+        self.assertLess(self.post.publish, self.now)
+        self.assertLess(self.post.updated, self.now)
+
+    def test_comment_creation(self):
+        self.assertLess(self.comment.created, self.now)
+        self.assertLess(self.comment.updated, self.now)
