78b5e181265d7ee443b52af1181f89a3819f1b43
ManuCiao <mn.sabatino@gmail.com>
2017-12-11 22:02:53 +0000

    Add more tests to 1 view

--- SmilesBlog/blog/tests.py
+++ SmilesBlog/blog/tests.py
@@ -1,3 +1,4 @@
+import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.core.urlresolvers import reverse
@@ -41,12 +42,14 @@ class BlogViewTests(TestCase):
             slug = "test-post-number-1",
             author = self.user,
             body = "Description of the Post test 1..."
+            #publish = datetime.datetime.today()
         )
         self.post2 = Post.objects.create(
             title = "Test Post number 2",
             slug = "test-post-number-1",
             author = self.user,
             body = "Description of the Post test 2..."
+            #publish = datetime.datetime.today()
         )
         self.comment = Comment.objects.create(
             post = self.post,
@@ -58,4 +61,7 @@ class BlogViewTests(TestCase):
         self.now = timezone.now()
 
     def test_blog_list_view(self):
-        resp = self.client.get(reverse('blog:list'))
+        resp = self.client.get(reverse('blog:post_list'))
+        self.assertEqual(resp.status_code, 200)
+        #self.assertEqual(self.post, resp.context['posts'])
+        #self.assertIn(self.post2, resp.context['posts'])
--- SmilesBlog/smilesblog/settings.py
+++ SmilesBlog/smilesblog/settings.py
@@ -99,7 +99,7 @@ ROOT_URLCONF = 'smilesblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['SmilesBlog/templates',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
